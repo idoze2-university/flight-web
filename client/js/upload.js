fe190706100379d6c80041d89c74053411618a41
idoz <ido.ze2@gmail.com>
2020-06-01 00:10:00 +0300

    created upload element and installed bootstrap, jQuery

---
+++ client/js/upload.js
@@ -0,0 +1,21 @@
+    let dropZone = $('#drop-zone');
+
+    let startUpload = function (files) {
+        console.log(files)
+    }
+
+    dropZone.ondrop = function (e) {
+        e.preventDefault();
+        this.className = 'upload-drop-zone';
+        startUpload(e.dataTransfer.files)
+    }
+
+    dropZone.ondragover = function () {
+        this.className = 'upload-drop-zone drop';
+        return false;
+    }
+
+    dropZone.ondragleave = function () {
+        this.className = 'upload-drop-zone';
+        return false;
+    }
