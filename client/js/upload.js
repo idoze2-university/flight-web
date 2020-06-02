1fb4658bfe4984249ef595b6ba618503b3de8964
idoz <ido.ze2@gmail.com>
2020-06-02 20:19:03 +0300

    Flight board design and upload css and jq

--- client/js/upload.js
+++ client/js/upload.js
@@ -3,25 +3,33 @@ $("html").on("dragover", function (e) {
     e.preventDefault();
     e.stopPropagation();
     $('#fw-upload').addClass('drop');
+    $('#fw-upload').innerHTML = "Drop the file anywhere to upload.";
 });
 $("html").on("drop", function (e) {
     e.preventDefault();
     e.stopPropagation();
+    $('#fw-upload').removeClass('drop');
+    // console.log(e);
+    ajax_upload(e.originalEvent.dataTransfer.files);
+});
+$("html").on("dragleave", function (e) {
+    $('#fw-upload').removeClass('drop');
 });
 
-let fwUpload = $('#fw-upload');
-
-let startUpload = function (files) {
-    console.log(files)
-}
-
-$("#fw-upload").on("drop", function (e) {
-    this.className = 'upload-drop-zone';
-    startUpload(e.dataTransfer.files)
-});
-
-fwUpload.ondragleave = function () {
-    this.className = 'upload-drop-zone';
-    return false;
+async function ajax_upload(files) {
+    var fd = new FormData();
+    fd.append('file', files);
+    await $.ajax({
+        url: `${server}/api/FlightPlan`,
+        type: 'POST',
+        data: fd,
+        success: function (response) {
+            if (response != 0) {
+                alert('file uploaded');
+            } else {
+                alert('file not uploaded');
+            }
+        },
+    });
 }
 
